e95e4fcbb01809ace90b385313422b86462db217
Morgan Waldron <morgan.waldron@versent.com.au>
2023-12-04 16:29:33 +1100

    Day 4, part 2

--- day_4.py
+++ day_4.py
@@ -13,7 +13,22 @@ def part_1(p_Input):
 
 
 def part_2(p_Input):
-    pass
+    wins = dict()
+    for game in p_Input.strip().splitlines():
+        game_no, game_data = game.split(': ')
+        game_no = parse_ints(game_no)[0]
+        winning_numbers, card_numbers = [set(parse_ints(x)) for x in game_data.split(' | ')]
+        wins[game_no] = len(card_numbers.intersection(winning_numbers))
+
+    cards = { x: 1 for x in wins }
+
+    for card in cards:
+        have_amount = cards[card]
+        win_amount = wins[card]
+        for i in range(card + 1, card + win_amount + 1):
+            if i in cards: cards[i] += have_amount
+
+    return sum(cards.values())
 
 
 example_input_1 = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
@@ -28,5 +43,5 @@ challenge_input = Input(4)
 assert(part_1(example_input_1) == 13)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1) == 30)
 print(f"Part 2: {part_2(challenge_input)}")
