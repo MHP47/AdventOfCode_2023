76112a635c57f5d6c08c00dab6ab66e69a94874f
Morgan Waldron <morgan.waldron@versent.com.au>
2023-12-04 14:23:57 +1100

    Day 3, part 2

--- day_3.py
+++ day_3.py
@@ -30,7 +30,38 @@ def part_1(p_Input):
 
 
 def part_2(p_Input):
-    pass
+    parts = re.findall(r'\d+', p_Input)
+    grid = Grid(rows=p_Input.strip().splitlines())
+    part_locs = dict()
+    real_parts = []
+    p_counter = 0
+
+    for h in range(grid.height):
+        w = 0
+        while w < grid.width:
+            v = grid[(w,h)]
+            if not v.isdigit():
+                w += 1
+                continue
+            part = parts[p_counter]
+            p_counter += 1
+            to_check = [(w+i,h) for i in range(len(part))]
+            w += len(part)
+            checked = [y for x in to_check for y in neighbors8(x) if y in grid and y not in to_check]
+            assert all([grid[x].isdigit() for x in to_check]), to_check
+            for i in to_check:
+                part_locs[i] = part
+            if any([not grid[x].isdigit() and grid[x] != '.' for x in checked]):
+                real_parts.append(part)
+
+    gears = [k for k,v in grid.items() if v == '*']
+    gear_ratio = 0
+    for gear in gears:
+        gear_set = set([part_locs[x] for x in neighbors8(gear) if x in grid and grid[x].isdigit()])
+        if len(gear_set) == 2:
+            gear_ratio += mul_reduce(map(int, gear_set))
+
+    return gear_ratio
 
 
 example_input_1 = """467..114..
@@ -49,5 +80,5 @@ challenge_input = Input(3)
 assert(part_1(example_input_1) == 4361)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1) == 467835)
 print(f"Part 2: {part_2(challenge_input)}")
