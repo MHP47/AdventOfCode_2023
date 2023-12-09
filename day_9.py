17f9fc7f46b0913ca238019a624bb3ab8871c160
Morgan Waldron <morgan.waldron@versent.com.au>
2023-12-09 17:24:03 +1100

    Day 9, part 2

--- day_9.py
+++ day_9.py
@@ -21,7 +21,14 @@ def part_1(p_Input):
 
 
 def part_2(p_Input):
-    pass
+    results = defaultdict(list)
+
+    for seq, nums in enumerate((parse_ints(x) for x in p_Input.splitlines())):
+        while sum(nums) != 0:
+            results[seq].append(nums[0])
+            nums = [-sub_reduce(x) for x in pairwise(nums)]
+
+    return sum([reduce((lambda x, y: y - x), z[::-1]) for z in results.values()])
 
 
 example_input_1 = """0 3 6 9 12 15
@@ -33,5 +40,5 @@ challenge_input = Input(9)
 assert(part_1(example_input_1) == 114)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1) == 2)
 print(f"Part 2: {part_2(challenge_input)}")
