3b9dcf937fe4c31ece9fda9f85615fe752a63b70
Morgan Waldron <morgan.waldron@versent.com.au>
2023-12-11 21:58:35 +1100

    Day 11, part 1

---
+++ day_11.py
@@ -0,0 +1,35 @@
+from utils import *
+
+
+def part_1(p_Input):
+    inpt = re.sub(f'(\.{{{len(p_Input.splitlines()[0])}}}\n)', r'\1\1', p_Input).splitlines()
+    grid = Grid(rows = inpt)
+    for i in [x for x in range(grid.width) if all([grid[(x,y)] == '.' for y in range(grid.height)])][::-1]:
+        for j in range(len(inpt)):
+            inpt[j] = inpt[j][:i] + '.' + inpt[j][i:]
+    grid = Grid(rows = inpt)
+    return sum(cityblock_distance(*y) for y in itertools.combinations([k for k,v in grid.items() if v == '#'], 2))
+
+
+def part_2(p_Input):
+    pass
+
+
+example_input_1 = """...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+"""
+challenge_input = Input(11)
+
+assert(part_1(example_input_1) == 374)
+print(f"Part 1: {part_1(challenge_input)}")
+
+assert(part_2(example_input_1) == None)
+print(f"Part 2: {part_2(challenge_input)}")
