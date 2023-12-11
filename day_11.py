1e48a6ee54d3a6c2e9bba18bfd738e835587d22a
Morgan Waldron <morgan.waldron@versent.com.au>
2023-12-11 22:14:36 +1100

    Day 11, part 2

--- day_11.py
+++ day_11.py
@@ -11,8 +11,21 @@ def part_1(p_Input):
     return sum(cityblock_distance(*y) for y in itertools.combinations([k for k,v in grid.items() if v == '#'], 2))
 
 
-def part_2(p_Input):
-    pass
+def part_2(p_Input, expansion=1000000):
+    grid = Grid(rows = p_Input.splitlines())
+
+    blank_cols = [x for x in range(grid.width) if all([grid[(x,y)] == '.' for y in range(grid.height)])]
+    blank_rows = [y for y in range(grid.height) if all([grid[(x,y)] == '.' for x in range(grid.width)])]
+
+    galaxies = [
+        (
+            x+(expansion-1)*sum([x>z for z in blank_cols]),
+            y+(expansion-1)*sum([y>z for z in blank_rows])
+        )
+        for x,y in [k for k,v in grid.items() if v == '#']
+    ]
+
+    return sum(cityblock_distance(*y) for y in itertools.combinations(galaxies, 2))
 
 
 example_input_1 = """...#......
@@ -31,5 +44,6 @@ challenge_input = Input(11)
 assert(part_1(example_input_1) == 374)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1, 10) == 1030)
+assert(part_2(example_input_1, 100) == 8410)
 print(f"Part 2: {part_2(challenge_input)}")
