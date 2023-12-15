560ebe494218fb1f74cbcedfd97975a6cd6be66c
Morgan Waldron <morgan.waldron@versent.com.au>
2023-12-15 22:42:02 +1100

    Day 14, part 2

--- day_14.py
+++ day_14.py
@@ -8,7 +8,7 @@ def part_1(p_Input):
     for r in rocks:
         while True:
             move = tuple(sum(x) for x in zip(UP, r))
-            if Y(r)==0 or g[move]in ('O','#'):
+            if Y(r)==0 or g[move] in ('O','#'):
                 break
             g[r] = '.'
             g[move] = 'O'
@@ -18,7 +18,50 @@ def part_1(p_Input):
 
 
 def part_2(p_Input):
-    pass
+    def tilt(records):
+        height = len(records)
+        width = len(records[0])
+        for x in range(width):
+            for _ in range(height):
+                for y in range(height):
+                    if records[y][x] == 'O' and y > 0 and records[y-1][x] == '.':
+                        records[y][x] = '.'
+                        records[y-1][x] = 'O'
+        return records
+
+    def rotate(records):
+        height = len(records)
+        width = len(records[0])
+        new_grid = [['x' for _ in range(height)] for _ in range(width)]
+        for y in range(height):
+            for x in range(width):
+                new_grid[x][height-1-y] = records[y][x]
+        return new_grid
+
+    g = [[r for r in row] for row in p_Input.strip().splitlines()]
+    cache = {}
+    counter = 0
+    while counter < 1000000000:
+        counter += 1
+        for _ in range(4):
+            g = tilt(g)
+            g = rotate(g)
+        hashed = tuple(tuple(row) for row in g)
+        if hashed in cache:
+            cycle_length = counter - cache[hashed]
+            step_amount = (1000000000 - counter) // cycle_length
+            counter += step_amount * cycle_length
+        cache[hashed] = counter
+
+    total = 0
+    height = len(g)
+    width = len(g[0])
+    for y in range(height):
+        for x in range(width):
+            if g[y][x] == 'O':
+                total += height - y
+
+    return total
 
 
 example_input_1 = """O....#....
@@ -37,5 +80,5 @@ challenge_input = Input(14)
 assert(part_1(example_input_1) == 136)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1) == 64)
 print(f"Part 2: {part_2(challenge_input)}")
